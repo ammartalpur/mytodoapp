39971809bd5e640db022887ecb540db19e53e664
Ammar Talpur <83033514+ammartalpur@users.noreply.github.com>
2023-07-22 12:49:52 +0500

    vercel final maybe :(

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path')
 
 const app = express()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 let newItem
@@ -69,6 +69,9 @@ db.on("error", console.error.bind(console, "Connection failed:"));
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname))
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "/public/"));
+
 
 
 app.get('/:customListName', (req, res) => {
